8b036108f152fece2b7cb5012ad8eed2c1ea9935
gopal-iosdev <gopal.iosdev@gmail.com>
2023-01-10 15:46:11 -0500

    Added Concurrency folder

---
+++ Concurrency/iOS Concurrency/README.md
@@ -0,0 +1,4 @@
+#  iOS Concurrency
+
+# Source: https://www.youtube.com/watch?v=S5PYdqfyroI&list=PLBn01m5Vbs4BKxvt7d4kyIr1ZUNlOdYTe&index=2&ab_channel=CodeWithChris
+
---
+++ Concurrency/iOS Concurrency/iOS Concurrency/iOS_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ConcurrencyApp.swift
+//  iOS Concurrency
+//
+//  Created by Gopal Gurram on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
