2a81b4d0d816672072bba2e53395ddd270d00cea
Gopal Gurram <gopal@epic-mbp.mynetworksettings.com>
2023-01-27 23:52:36 -0500

    Performance measurement using ContinuousClock

---
+++ Performance/PerformanceMeasurement.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,29 @@
+//: [Previous](@previous)
+
+/*
+ 
+ Link:
+    - https://dylancfe15.medium.com/swift-measuring-performance-8fe923469d98
+ 
+ 
+ - Using ContinuousClock:
+    - iOS 16+
+    -
+ 
+*/
+
+
+import Foundation
+
+func doSomething() {
+    for number in 0..<10_000_000 {
+        // do something
+    }
+}
+
+let clock = ContinuousClock( )
+let time = clock.measure(doSomething)
+print(time)
+
+
+//: [Next](@next)
