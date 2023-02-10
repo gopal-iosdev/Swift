b851da516a00d5b255a5456f45e597aa4c993d8f
gopal-iosdev <gopal.iosdev@gmail.com>
2023-02-10 14:11:46 -0500

    Multi Threading changes 1

--- Concurrency/Concurrency.playground/Pages/Thread.xcplaygroundpage/Contents.swift
+++ Concurrency/Concurrency.playground/Pages/Thread.xcplaygroundpage/Contents.swift
@@ -29,6 +29,7 @@
 
 import Foundation
 
+/*
 extension Thread {
     var threadName: String {
         if let currentOperationQueue = OperationQueue.current?.name {
@@ -46,6 +47,63 @@ print(Thread().threadName)
 DispatchQueue.global(qos: .background).async {
     print(Thread().threadName)
 }
+ */
 
+// MARK: - 1.) What is a Thread?
+
+/*
+ - Create a new Xcode project and pause execution.
+ - Show all the available threads.
+ */
+
+// MARK: - 2.) How to create a Thread?
+
+class SomeApp {
+    var thread: Thread?
+    
+    func createThread() {
+        thread = Thread(target: self, selector: #selector(fetchData),  object: nil)
+                                    
+        thread?.start()
+    }
+    
+    func cancelThread() {
+        thread?.cancel()
+    }
+    
+    @objc func fetchData() {
+        dump("Custom thread in action")
+    }
+}
+
+let customThread = SomeApp()
+customThread.createThread()
+print(customThread.thread?.isExecuting, customThread.thread?.isFinished, customThread.thread?.isCancelled)
+customThread.cancelThread()
+print(customThread.thread?.isExecuting, customThread.thread?.isFinished, customThread.thread?.isCancelled)
+
+// MARK: - 3.) Why do we need Threads?
+
+/*
+ - Threads are an essential tool for improving the performance, responsiveness, and overall user experience of iOS applications. They help you achieve concurrency, parallelism, and background processing, all of which are crucial for modern mobile applications.
+ - Run Main Thread App & Show Main Thread v/s Background Thread example.
+ - Run Instruments -> Animation Hitches and Time Profiler to show the performance difference.
+ */
+
+// MARK: - 4.) How do we manage Threads => Multi Threading/ Concurrency
+
+/*
+ - Creating and managing threads manually.
+ - Using GCD (Grand Central Dispatch).
+ - Operations and queues (NSOperation).
+ - Combine
+ - Async Await.
+ */
+
+// MARK: - 5.) Using GCD (Grand Central Dispatch)
+
+/*
+ - Switch to GCD playground page
+ */
 
 //: [Next](@next)
